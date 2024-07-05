0c6638aefec493096180ff3cdcd2a027f699ac1f
Vlad <popicavlas@gmail.com>
2024-07-05 20:51:58 +0300

    Seting the connection with database MySql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,34 +2,20 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import { connectToDatabase } from './connect';
+
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-const dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-};
 
-const connectToDatabase = async () => {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    console.log('Connected to MySQL database');
-    return connection;
-  } catch (error) {
-    console.error('Error connecting to MySQL database:', error);
-    throw error;
-  }
-};
 
 app.get('/', async (req, res) => {
   try {
     const connection = await connectToDatabase();
-    const [rows] = await connection.execute('SELECT * FROM test_table');
+    const [rows] = await connection.execute('SELECT * FROM users');
     res.json(rows);
   } catch (error) {
     res.status(500).json({ error: 'Database connection failed' });
